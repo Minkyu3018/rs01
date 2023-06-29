0528c2bb8926e7c5d9cb5185514c30143f20788b
Minkyu3018 <koreamanggu@gmail.com>
2023-06-29 13:10:32 +0900

    06 29 컴퍼넌트 나눠서 키오스크 및 카운터

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Counter from "./components/Counter";
 import Counter2 from "./components/Counter2";
 import Kiosk from "./components/Kiosk";
+import ZCounter from "./components/zcounter";
+import ZKiosk from "./components/zkiosk";
 
 function App() {
   return (
-    <div className="bg-stone-950">
-      <div className="w-full h-24 text-zinc-50 border-b-2 border-cyan-50 ">
-          <img className="w-20 h-20 m-auto pt-3" src="coffe.png" />
-      </div>  
-      <Kiosk></Kiosk>
+    <div>
+      {/* <div className="w-full h-24 bg-sb-gary2 border-b-2 border-cyan-50 ">
+          <img className="w-20 h-20 m-auto pt-3" src="logo2.png" />
+      </div>   */}
+        {/* <ZCounter/> */}
+        <ZKiosk></ZKiosk>
 
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sb-green' : 'bg-white',
+        'sb-gray1' : '#969691',
+        'sb-gary2' : '#F1ECE1'
+      }
+    },
   },
   plugins: [],
 }
 
+// #1b3c35
